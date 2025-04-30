e8228011d7c5316001a01a225ec2d5bcbc3d9901
cyhofe <cyrill.hofer@uzh.ch>
2025-04-30 15:31:01 +0200

    Install and update .env.example

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
 
-st.title("Medical Shift Planner")
-st.write("Welcome to the planner!")
+load_dotenv()  # Load from .env
+
+st.set_page_config(page_title=os.getenv("APP_TITLE", "Planner"))
+st.title(os.getenv("APP_TITLE"))
+
+st.write("Database is at:", os.getenv("DATABASE_URL"))
