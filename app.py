68d0229cec8fea307d1e1efab41dec74d64e30d1
cyhofe <cyrill.hofer@uzh.ch>
2025-05-02 14:08:49 +0200

    Adapt local database

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Medical Shift Planner")
 st.write("Welcome to the planner!")
+
+
+# start and input the database
+from code.utils.db import initialize_db
+
+initialize_db()
