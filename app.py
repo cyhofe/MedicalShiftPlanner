f76463051917e409b539f28706020b3e11f6b059
cyhofe <cyrill.hofer@uzh.ch>
2025-05-02 13:31:09 +0200

    Additional changes Setup Development Platform

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
-load_dotenv()  # Load from .env
-
-st.set_page_config(page_title=os.getenv("APP_TITLE", "Planner"))
-st.title(os.getenv("APP_TITLE"))
-
-st.write("Database is at:", os.getenv("DATABASE_URL"))
+st.title("Medical Shift Planner")
+st.write("Welcome to the planner!")
