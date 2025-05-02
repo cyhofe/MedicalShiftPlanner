77614c7edd3ae69aa0d02b3dafa52a6d7339a6f6
cyhofe <cyrill.hofer@uzh.ch>
2025-05-02 15:41:17 +0200

    keep app.py simple

--- app.py
+++ app.py
@@ -2,9 +2,3 @@ import streamlit as st
 
 st.title("Medical Shift Planner")
 st.write("Welcome to the planner!")
-
-
-# start and input the database
-from code.utils.db import initialize_db
-
-initialize_db()
